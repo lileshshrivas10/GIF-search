41c9e30d4cb27eca1a73c880eb4127a9a02c1644
Lilesh Shrivas <lileshshrivas10@gmail.com>
2022-05-20 17:15:26 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { GifAPIServiceService } from './services/gif-apiservice.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'GIF-search';
+  gifs = [1,2,3,4,5,6,7,8,9,10];
+  gifData:any[] = [];
+  searchTerm = "";
+  constructor(private API:GifAPIServiceService){
+    this.getGIFs("dog");
+    // default search value
+  }
+  performSearch(searchTerm: HTMLInputElement): void {
+    this.searchTerm = searchTerm.value;
+    console.log(`User entered: ${searchTerm.value}`);
+    this.getGIFs(searchTerm.value);
+}
+
+getGIFs(searchTerm:string){this.API.getGIFs(searchTerm).subscribe(e => {
+  this.gifData  = e['data'];
+  console.log(this.gifData)
+});
+}
+}
+
